dc4e143fed177e18393581a2b10012b223b46993
Innuendo416 <collinstone2291@icloud.com>
2020-09-17 15:45:54 -0400

    ...

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ if(myPort === 3000){
 
 if(process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'))
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
     	res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
